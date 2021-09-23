b3dc206afa4d2793e68ec3ff2e72a42b589f169b
william <1243650225@qq.com>
2021-09-23 22:25:22 +0800

    add is_store_access_token

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import wxnotify
 
 # 企业ID
-CORPID = 'wwfaa4*******e5fd7'
+CORPID = 'wwfaa********1ae5fd7'
 # 应用Secret
-CORPSECRET = '92XHn****************wRiUgnTx1A'
+CORPSECRET = '92XHn***************RiUgnTx1A'
 AgentId = '1000002'
 
-t = wxnotify.WxNotify(CORPID,CORPSECRET,AgentId)
-res = t.send('this is a message')
+t = wxnotify.WxNotify(CORPID,CORPSECRET,AgentId,False) # 最后一个参数是否本地缓存access_token
+res = t.send('this is a message 1241552')
 print(res)
 
 res = t.send_picture('example.png')
--- wxnotify/notify.py
+++ wxnotify/notify.py
@@ -1,19 +1,39 @@
 import requests
 import json
+import time
+from functools import wraps
+
+
+def cheak_access_token(f):
+    """ 拦截需要access_token的函数添加access_token参数 """
+    @wraps(f)
+    def wrapper(*arg,**kwargs):
+        if len(arg) < 3:
+            assert isinstance(arg[0],WxNotify)
+            result = f(*arg,arg[0].get_access_token(),**kwargs)
+            return result
+        else:
+            raise
+    return wrapper
 
 
 class WxNotify:
-    def __init__(self, corpid, corpsecret, agentid):
+    def __init__(self, corpid, corpsecret, agentid,is_store_access_token):
+        """ 
+        is_store_access_token    是否本地缓存 access_token 不缓存适合云函数等存储困难的场景使用
+        """
         self.corpid = corpid
         self.corpsecret = corpsecret
         self.agentid = agentid
-        self.access_token = self.__get_access_token(corpid, corpsecret)
+        self.is_store_access_token = is_store_access_token
+        self.access_token = None
+        self.access_start_time = None
 
-    def __get_access_token(self, corpid, corpsecret):
+    def __get_access_token(self):
         url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
         params = {
-            'corpid': corpid,
-            'corpsecret': corpsecret
+            'corpid': self.corpid,
+            'corpsecret': self.corpsecret
         }
         resp = requests.get(url, params=params)
         resp.raise_for_status()
@@ -24,10 +44,55 @@ class WxNotify:
             raise Exception(
                 'Please check if corpid and corpsecret are correct \n'+resp.text)
 
-    def upload_picture(self, pic_path):
+    def get_access_token(self):
+        """ 
+        启用本地缓存 access_token 的情况
+        先从access_token.conf 获取
+        计算是否过期，过期重新获取
+        """
+
+        if self.is_store_access_token:
+            # 本地缓存的情况
+            try:
+                with open('access_token.conf', 'r') as f:
+                    t, access_token = f.read().split()
+            except:
+                with open('access_token.conf', 'w') as f:
+                    access_token = self.__get_access_token()
+                    cur_time = time.time()
+                    f.write('\t'.join([str(cur_time), access_token]))
+                    return access_token
+            else:
+                cur_time = time.time()
+                if 0 < cur_time - float(t) < 7200:#token的有效时间7200s
+                    return access_token
+                else:
+                    with open('access_token.conf', 'w') as f:
+                        access_token = self.__get_access_token()
+                        f.write('\t'.join([str(cur_time), access_token]))
+                        return access_token
+        else:
+            # 本地不缓存的情况
+            if not self.access_token:
+                # self存一份返回一份
+                self.access_token = self.__get_access_token()
+                self.access_start_time = time.time()
+                return self.access_token
+            else:
+                cur_time = time.time()
+                if 0 < cur_time - float(self.access_start_time) < 7200:#token的有效时间7200s
+                    return self.access_token
+                else:
+                    self.access_token = self.__get_access_token()
+                    self.access_start_time = time.time()
+                    return self.access_token
+
+    @cheak_access_token
+    def upload_picture(self, pic_path,access_token): 
+        # access_token = self.access_token if self.access_token else self.__get_access_token()
         url = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload'
         params = {
-            'access_token': self.access_token,
+            'access_token':access_token,
             'type': 'image'
         }
         files = {
@@ -37,9 +102,11 @@ class WxNotify:
         resp.raise_for_status()
         return resp.json()["media_id"]
 
-    def send_picture(self, pic_path):
+    @cheak_access_token
+    def send_picture(self, pic_path,access_token):
+        # access_token = self.access_token if self.access_token else self.__get_access_token()
         media_id = self.upload_picture(pic_path)
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='+self.access_token
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='+ access_token
         payload = {
             "touser": "@all",
             "msgtype": "image",
@@ -53,9 +120,11 @@ class WxNotify:
         resp.raise_for_status()
         return resp.json()
 
-
-    def send(self, text):
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='+self.access_token
+    @cheak_access_token
+    def send(self, text,access_token):
+        """ 强行使用装饰器 """
+        # access_token = self.get_access_token()
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='+ access_token
         payload = {
             "touser": "@all",
             "msgtype": "text",
@@ -74,8 +143,10 @@ class WxNotify:
 
 
 if __name__ == "__main__":
-    CORPID = 'wwfaa*********'
-    CORPSECRET = '***********ZgYcf5QGUYLc5YK-********'
-    AgentId = '******'
-    wn = WxNotify(corpid=CORPID, corpsecret=CORPSECRET, agentid=AgentId)
+    # 企业ID
+    CORPID = ''
+    # 应用Secret
+    CORPSECRET = ''
+    AgentId = '1000002'
+    wn = WxNotify(corpid=CORPID, corpsecret=CORPSECRET, agentid=AgentId,is_store_access_token=False)
     print(wn.send("test message"))
